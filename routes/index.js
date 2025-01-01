7116a9334181796b79b8f19c47ac3a2bcff9aeb3
mrsajadpp <mrsajadpp@gmail.com>
2025-01-01 15:03:13 +0530

    sitemap.xm added

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,50 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Sitemap route
+router.get('/sitemap.xml', (req, res) => {
+    try {
+        const urls = [
+            { loc: 'https://refrut.grovixlab.com/', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/about', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/auth/login', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/privacy-policy', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/terms-and-conditions', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/auth/reset-password', priority: 1.0 },
+            { loc: 'https://refrut.grovixlab.com/auth/signup', priority: 1.0 },
+        ];
+
+        const currentDate = new Date().toISOString();
+
+        let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset
+        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">\n`;
+
+        urls.forEach((url) => {
+            sitemap += `
+        <url>
+          <loc>${url.loc}</loc>
+          <lastmod>${currentDate}</lastmod>
+          <changefreq>daily</changefreq>
+          <priority>${url.priority.toFixed(2)}</priority>
+        </url>`;
+        });
+
+        sitemap += '\n</urlset>';
+
+        // Set response header to XML
+        res.header('Content-Type', 'application/xml');
+        res.send(sitemap);
+    } catch (error) {
+        console.error(error);
+        logger.logError(error);
+        res.status(500).send('Server error');
+    }
+});
+
 // Route for robots.txt
 router.get('/robots.txt', (req, res) => {
     try {
