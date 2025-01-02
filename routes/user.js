bffedcd914b147fcf204db33370a6018e34e47f4
mrsajadpp <mrsajadpp@gmail.com>
2025-01-02 12:10:49 +0530

    feat: enhance user dashboard with referral tracking and responsive design

--- routes/user.js
+++ routes/user.js
@@ -9,23 +9,23 @@ const moment = require('moment');
 router.get('/', async (req, res) => {
     try {
         let user = await User.findOne({ _id: new mongoose.Types.ObjectId(req.session.user._id) });
+        let referrals = await User.find({ reffer_user: user._id }); // Changed to find all referrals
         const originalExpiryDate = moment(user.accountExpiryDate).format('DD/MM/YYYY');
         const updatedExpiryDate = moment(user.accountExpiryDate).add(1, 'month').format('DD/MM/YYYY');
         res.render('user/app', {
             title: "Refrut",
             metaDescription: 'Welcome to Refrut, a dynamic community for startups, tech enthusiasts, and innovators. Discover resources, connect with like-minded professionals, and unlock new opportunities to grow.',
-            error: null, message: null, auth_page: true, req: req, originalExpiryDate, updatedExpiryDate
+            error: null, message: null, auth_page: true, req: req, originalExpiryDate, updatedExpiryDate, user, referrals
         });
     } catch (error) {
         logger.logError(error);
         res.render('user/app', {
             title: "Refrut",
             metaDescription: 'Welcome to Refrut, a dynamic community for startups, tech enthusiasts, and innovators. Discover resources, connect with like-minded professionals, and unlock new opportunities to grow.',
-            error: 'Server Error', message: null, auth_page: true, req: req, originalExpiryDate, updatedExpiryDate
+            error: 'Server Error', message: null, auth_page: true, req: req, originalExpiryDate: null, updatedExpiryDate: null, user: null, referrals: null 
         });
     }
 });
-
 
 router.get('/logout', (req, res) => {
     req.session = null;
